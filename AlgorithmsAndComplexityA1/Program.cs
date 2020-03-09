6cad1f56b612451a4a9cb83e79f9115c71701f27
technician <technician@lincoln.ac.uk>
2020-03-09 12:30:58 +0000

    Added file selection

--- AlgorithmsAndComplexityA1/Program.cs
+++ AlgorithmsAndComplexityA1/Program.cs
@@ -2,6 +2,10 @@
 using System.IO;
 using System.Net;
 using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace AlgorithmsAndComplexityA1
 {
@@ -9,26 +13,32 @@ namespace AlgorithmsAndComplexityA1
     {
         static void Main(string[] args)
         {
+            int[] networkDataFromFile = {}; 
+            List<String> NetworkDataPaths = new List<String>();
 
             //Get the current path - ensuring the program still runs on a different system
             string currentPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             // Loop until network data is selected - handle errors
-            foreach (var file in Directory.EnumerateFiles(currentPath+"/NetworkData"))
+            foreach (var file in Directory.EnumerateFiles(currentPath + "/NetworkData"))
             {
-                Console.WriteLine(file);
+                //Store path for reference in 
+                NetworkDataPaths.Add(file);
             }
-                //Handle selection
+            //Handle selection
+            Console.WriteLine("Available Network Data files: ");
+            int count = 0;
+            foreach (string path in NetworkDataPaths)
+            {
+                count++;
+                string[] pathSplit = path.Split('/');
+                Console.WriteLine("[" + count.ToString() + "] " + pathSplit[pathSplit.Length-1]);
+            }
+            Console.Write("Select file: ");
+            string fileSelection = Console.ReadLine(); // ADD ERROR HANDLING HERE
+            Console.WriteLine(fileSelection);
+            Console.WriteLine(NetworkDataPaths[Convert.ToInt32(fileSelection)-1]);
+
+
         }
     }
 }
-
-
-    // __     __
-    //(_\    /_ )
-    // \ _\  /_ / 
-    //  \ _\/_ /_ _
-    //  |_____/_/ /|
-    //  ((_) __) J-)
-    //  (  /`.,   /
-    //   \/  ;   /
-    //   | === |
---
+++ AlgorithmsAndComplexityA1/Searches.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsAndComplexityA1
+{
+    class Searches
+    {
+    }
+}
